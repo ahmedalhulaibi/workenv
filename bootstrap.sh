2d48e96a719e664be81eaf9247f71222847f3e39
ahmedalhulaibi <ahmed.alhulaibi41@gmail.com>
2020-09-08 11:27:17 -0400

    install docker-compose

--- bootstrap.sh
+++ bootstrap.sh
@@ -190,6 +190,11 @@ if ! [ -x "$(command -v heroku)" ]; then
   popd
 fi
 
+if ! [ -x "$(command -v docker-compose)" ]; then
+  sudo curl -L --fail https://github.com/docker/compose/releases/download/1.27.0/run.sh -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+
 # install tools
 if ! [ -x "$(command -v jump)" ]; then
   echo " ==> Installing jump .."
